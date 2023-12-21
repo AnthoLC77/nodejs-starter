27eff15824bdae41b653c4554038c3e98ea5047a
AnthoLC77 <mishynaro@hotmail.fr>
2023-12-21 14:11:04 +0100

    mise en place du localhost

--- server.js
+++ server.js
@@ -2,3 +2,27 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+
+// Middleware
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Afficher hello world
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Lancement du serveur
+const PORT = process.env.PORT || 5600;
+
+// Definir une fonction pour lancer le serveur
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
